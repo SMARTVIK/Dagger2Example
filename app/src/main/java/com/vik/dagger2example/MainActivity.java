bd94ce9229bad5853fc3fdb1e64274a203211ec3
smartvik <vps631532@gmail.com>
2020-08-10 17:19:49 +0530

    Field injection done

--- app/src/main/java/com/vik/dagger2example/MainActivity.java
+++ app/src/main/java/com/vik/dagger2example/MainActivity.java
@@ -4,22 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 import dagger.internal.DaggerCollections;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Inject
     Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         CarComponent carComponent = DaggerCarComponent.create();
-        car = carComponent.getCar();
+        carComponent.inject(this);
         car.drive();
-
-
     }
 }
